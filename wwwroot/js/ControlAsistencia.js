24d6ff51adca54a15a0ea364fb9bc45d78e438fd
esteban.lucas <esteban.lucas@xcf.com.mx>
2025-01-29 15:36:37 -0600

    Se cambia metodo de comparacion de imagenes

--- wwwroot/js/ControlAsistencia.js
+++ wwwroot/js/ControlAsistencia.js
@@ -498,7 +498,6 @@ const capturarImagen = async () => {
         quitLoadingButton("#Registrar")
         return false;
     }
-   
     // Enviar la imagen al servidor
     try {
         const response = await fetch($("#urlGuardarFoto").data("action-url"), {
@@ -512,7 +511,8 @@ const capturarImagen = async () => {
                 Ruta: ruta,
                 Rfc: rfc,
                 ClaveEmpleado: ClaveEmpleado,
-                TipoAsistencia: seleccionado
+                TipoAsistencia: seleccionado,
+                IdGeocerca: IdCentroServicio.toString()
             })
         });
         if (response.ok) {
